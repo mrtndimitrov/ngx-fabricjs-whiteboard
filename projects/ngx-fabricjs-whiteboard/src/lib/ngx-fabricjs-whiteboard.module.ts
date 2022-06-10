55ecaaef4970bcd577ceb3528fb0d84b27f10391
Martin Koroudjiev <martin@prosveta.bg>
2022-06-10 19:46:29 +0300

    Martin <mrtndimitrov@gmail.com>

--- projects/ngx-fabricjs-whiteboard/src/lib/ngx-fabricjs-whiteboard.module.ts
+++ projects/ngx-fabricjs-whiteboard/src/lib/ngx-fabricjs-whiteboard.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxFabricjsWhiteboardComponent } from './ngx-fabricjs-whiteboard.component';
-import { MatButtonModule } from '@angular/material/button';
-
-
 
 @NgModule({
   declarations: [
     NgxFabricjsWhiteboardComponent
   ],
-  imports: [
-    MatButtonModule
-  ],
+  imports: [],
   exports: [
     NgxFabricjsWhiteboardComponent
   ]
